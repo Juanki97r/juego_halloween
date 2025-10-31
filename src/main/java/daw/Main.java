597045cfcf9aae2456b27a726b76a6811197257d
Juan Carlos <juanki97r@gmail.com>
2025-10-31 09:42:07 +0100

    situacion horrible

--- src/main/java/daw/Main.java
+++ src/main/java/daw/Main.java
@@ -6,6 +6,27 @@ import javax.swing.JOptionPane;
 
 public class Main {
     public static void main(String[] args) {
+        String situacion1= """
+                Ha aparecido Doraemon detras tuya
+                adivina el número en el que esta pensando
+                o te meterá en su bolsillo mágico
+                """;
+        String situacion2 = """
+                Cuidado, ha aparecido Vico con un examen
+                sorpresa a primera hora de la mañana, si no
+                aciertas el número te espera un suspenso seguro
+                """;
+        String situacion3 = """
+                Te ha salido Antonio Lobato desde una esquina
+                ¡si no quieres saber cuanto vale tu coche acierta
+                el número y escapa!
+                """;
+        String situacion4 = """
+                ¡CUIDADO! ha aparecido un cryptobro
+                y pretende venderte su curso de emprendimiento,
+                más vale que aciertes el número secreto si no quieres
+                llegar a tu primer millón con billetes del monopoli
+                """;
         
         Juego.menu();
         int apertura = Juego.puertaOVentana();
@@ -28,14 +49,20 @@ public class Main {
                     int suerte = Juego.escapaOjuega();
                     if(suerte <= 50){
                         JOptionPane.showMessageDialog(null, "Es tu día de suerte, el fantasma esta dormido y has escapado");
+                        break;
                     }
+
+                    JOptionPane.showMessageDialog(null, situacion1);
                 }
                 case 2-> {
                     JOptionPane.showMessageDialog(null, "Has abierto la puerta del pasillo...");
                     int suerte = Juego.escapaOjuega();
                     if(suerte <= 50){
                         JOptionPane.showMessageDialog(null, "Es tu día de suerte, el fantasma esta dormido y has escapado");
+                        break;
                     }
+
+                    JOptionPane.showMessageDialog(null, situacion2);
                 }
                }
             }
@@ -57,7 +84,10 @@ public class Main {
                         int suerte = Juego.escapaOjuega();
                     if(suerte <= 50){
                         JOptionPane.showMessageDialog(null, "Es tu día de suerte, el fantasma esta dormido y has escapado");
+                        break;
                     }
+
+                        JOptionPane.showMessageDialog(null, situacion3);
                     }
 
                     case 2 -> {
@@ -65,8 +95,12 @@ public class Main {
                         int suerte = Juego.escapaOjuega();
                     if(suerte <= 50){
                         JOptionPane.showMessageDialog(null, "Es tu día de suerte, el fantasma esta dormido y has escapado");
+                        break;
                     }
-                    
+
+                        JOptionPane.showMessageDialog(null, situacion4);
+
+
                     }
             }
         }
